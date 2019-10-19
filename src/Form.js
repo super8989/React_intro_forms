97d33b68d4ae9ef9f41c9e503d140e3e919a7e5b
Sam <super8989@gmail.com>
2019-10-19 19:30:43 -0400

    form to show changing state as it's being typed

--- src/Form.js
+++ src/Form.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Form extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Form Demo</h1>
-      </div>
-    )
-  }
+	constructor(props) {
+		super(props);
+		this.state = { username: "" };
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(evt) {
+		this.setState({ username: evt.target.value });
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Form Demo</h1>
+				<form>
+					<input
+						type='text'
+						value={this.state.username}
+						onChange={this.handleChange}
+					/>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default Form;
