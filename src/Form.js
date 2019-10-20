5daf67b677bf516b9122cf2f7d885c8288d58d23
Sam <super8989@gmail.com>
2019-10-19 20:17:21 -0400

    added in submitting form

--- src/Form.js
+++ src/Form.js
@@ -5,22 +5,30 @@ class Form extends Component {
 		super(props);
 		this.state = { username: "" };
 		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
 	handleChange(evt) {
 		this.setState({ username: evt.target.value });
 	}
 
+	handleSubmit(evt) {
+		evt.preventDefault();
+		alert(`You typed: ${this.state.username}`);
+		this.setState({ username: "" });
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Form Demo</h1>
-				<form>
+				<form onSubmit={this.handleSubmit}>
 					<input
 						type='text'
 						value={this.state.username}
 						onChange={this.handleChange}
 					/>
+					<button>Submit!</button>
 				</form>
 			</div>
 		);
