50b9b538a4a207b5fdeb2f9f6bf102fc08bcaacb
Sam <super8989@gmail.com>
2019-10-19 23:22:34 -0400

    made MultipleForm to be the same as Form for initial demo

--- src/MultipleForm.js
+++ src/MultipleForm.js
@@ -4,14 +4,30 @@ class MultipleForm extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { username: "" };
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(evt) {
+		this.setState({ username: evt.target.value });
+	}
+
+	handleSubmit(evt) {
+		evt.preventDefault();
+		alert(`You typed: ${this.state.username}`);
+		this.setState({ username: "" });
 	}
 
 	render() {
 		return (
 			<div>
-				<form>
+				<form onSubmit={this.handleSubmit}>
 					<h1>Multiple Form Demo</h1>
-					<input />
+					<input
+						type='text'
+						value={this.state.username}
+						onChange={this.handleChange}
+					/>
 					<button>Submit!</button>
 				</form>
 			</div>
